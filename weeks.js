7a4894589bac3e8bed4360db8fd6dd15f2e9fd2a
JG <gadennejwork@gmail.com>
2025-09-10 17:06:09 +0800

    Update weeks.js

--- weeks.js
+++ weeks.js
@@ -1,7 +1,7 @@
 window.CUP_WEEKS = [
   {
     week: 1,
-    slug: 'week-1',
+    slug: 'week-01',
     title: 'The Big Kick-Off',
     blurb: 'Standings, trades, waivers, and matchup autopsies for the first week of the season.'
   }
