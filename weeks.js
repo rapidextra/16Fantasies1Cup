c373f9b901dc98a499dfcfa43b4e296dbf88c98e
JG <gadennejwork@gmail.com>
2025-09-21 14:00:24 +0800

    Update weeks.js

--- weeks.js
+++ weeks.js
@@ -1,5 +1,11 @@
 window.CUP_WEEKS = [
-  {
+ {
+    week: 2,
+    slug: 'week-2',
+    title: 'Is It Time To Panic Yet?',
+    blurb: 'The contenders start to pull away from the pretenders as several teams go 2-0, leaving others in an early-season panic.'
+  },  
+{
     week: 1,
     slug: 'week-01',
     title: 'The Big Kick-Off',
